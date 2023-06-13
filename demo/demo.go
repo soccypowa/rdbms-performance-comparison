b32de8b8b582c70a2e0f0fe9e252972279716c53
Sergey Olontsev <sergey@olontsev.io>
2023-06-13 19:15:10 +0300

    Demo program refactoring.

---
+++ demo/demo.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/solontsev/rdbms-performance-comparison/config"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	_ "github.com/microsoft/go-mssqldb"
+)
+
+func Ping(ctx context.Context, db *sql.DB) {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+}
+
+func ExecQuery(ctx context.Context, f func(context.Context, *sql.DB, string), db *sql.DB, query string, execs int) time.Duration {
+	for i := 0; i < config.WarmUpExecutions; i++ {
+		f(ctx, db, query)
+	}
+
+	if execs == 0 {
+		execs = config.TestExecutions
+	}
+	start := time.Now()
+	for i := 0; i < execs; i++ {
+		f(ctx, db, query)
+	}
+	return time.Since(start)
+}
+
+var tests = flag.String("tests", "01,02", "coma separated list of test keys")
+
+func main() {
+	var err error
+	var db *sql.DB
+
+	flag.Parse()
+
+	ctx, stop := context.WithCancel(context.Background())
+	defer stop()
+
+	appSignal := make(chan os.Signal, 3)
+	signal.Notify(appSignal, os.Interrupt)
+
+	go func() {
+		<-appSignal
+		stop()
+	}()
+
+	databases := []struct {
+		connectionName string
+		driverName     string
+		dsn            string
+	}{
+		{MySql, "mysql", "root:mysql@tcp(127.0.0.1:3306)/test_db"},
+		{PostgreSql, "postgres", "postgres://postgres:postgres@localhost:5432/test_db?sslmode=disable"},
+		{MsSql22, "sqlserver", "sqlserver://SA:myStrong(!)Password@localhost:1433?database=test_db"},
+		{MsSql19, "sqlserver", "sqlserver://SA:myStrong(!)Password@localhost:1434?database=test_db"},
+	}
+
+	var testCodes []string
+	if *tests == "all" {
+		testCodes = make([]string, len(Tests))
+		i := 0
+		for k := range Tests {
+			testCodes[i] = k
+			i++
+		}
+	} else {
+		testCodes = strings.Split(*tests, ",")
+		for i := range testCodes {
+			testCodes[i] = strings.TrimSpace(testCodes[i])
+		}
+	}
+
+	result := make(map[string]map[string]string, len(testCodes))
+
+	for _, d := range databases {
+		log.Printf("Running queries in %s database...", d.connectionName)
+		db, err = sql.Open(d.driverName, d.dsn)
+		if err != nil {
+			log.Fatalf("Unable to connect to database(%s): %v", d.connectionName, err)
+		}
+
+		db.SetConnMaxLifetime(0)
+		db.SetMaxIdleConns(3)
+		db.SetMaxOpenConns(3)
+		Ping(ctx, db)
+
+		for _, testCode := range testCodes {
+			testData, ok := Tests[testCode]
+			if !ok {
+				continue
+			}
+
+			queries, ok := testData.queries[d.connectionName]
+			if !ok {
+				continue
+			}
+			log.Printf("- %s - %s test...", testCode, testData.testName)
+
+			key := fmt.Sprintf("%s - %s", testCode, testData.testName)
+			_, ok = result[key]
+			if !ok {
+				result[key] = make(map[string]string)
+			}
+
+			for queryName, sqlText := range queries {
+				log.Printf("  - %s", queryName)
+				numberOfExecutions := config.TestExecutions
+				if testData.execCount > 0 {
+					numberOfExecutions = testData.execCount
+				}
+				duration := ExecQuery(ctx, testData.f, db, sqlText, numberOfExecutions)
+				subKey := d.connectionName
+				if queryName != "" {
+					subKey += fmt.Sprintf(" (%s)", queryName)
+				}
+				result[key][subKey] = fmt.Sprintf("%s", duration)
+			}
+		}
+
+		err := db.Close()
+	}
+
+	prettyResult, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		log.Println("error:", err)
+	}
+
+	log.Println(string(prettyResult))
+}
--- demo/queries.go
+++ demo/queries.go
@@ -1,4 +1,4 @@
-package demo
+package main
 
 import (
 	"context"
@@ -7,6 +7,11 @@ import (
 	"time"
 )
 
+const MySql = "mysql"
+const PostgreSql = "postgres"
+const MsSql19 = "mssql-19"
+const MsSql22 = "mssql-22"
+
 type testData struct {
 	testName  string
 	queries   map[string]map[string]string
@@ -14,109 +19,155 @@ type testData struct {
 	execCount int
 }
 
-var tests = []testData{
-	//{
-	//	"01 - lookup by primary key",
-	//	map[string]map[string]string{
-	//		"mysql": {
-	//			//"first key":                         "select id from client as c where id = 0;",
-	//			//"middle key":                        "select id from client as c where id = 5000;",
-	//			//"last key":                          "select id from client as c where id = 9999;",
-	//			//"not existing key at the beginning": "select id from client as c where id = 100000;",
-	//			//"not existing key at the end":       "select id from client as c where id = 100000;",
-	//			"lookup_and_agg": "select count(*) from order_detail as od where order_id = 1;",
-	//		},
-	//		"postgres": {
-	//			//"first key":                         "select id from client as c where id = 0;",
-	//			//"middle key":                        "select id from client as c where id = 5000;",
-	//			//"last key":                          "select id from client as c where id = 9999;",
-	//			//"not existing key at the beginning": "select id from client as c where id = 100000;",
-	//			//"not existing key at the end":       "select id from client as c where id = 100000;",
-	//			"lookup_and_agg": "select count(*) from order_detail as od where order_id = 1;",
-	//		},
-	//		"mssql-22": {
-	//			//"first key":                         "select id from client as c where id = 0;",
-	//			//"middle key":                        "select id from client as c where id = 5000;",
-	//			//"last key":                          "select id from client as c where id = 9999;",
-	//			//"not existing key at the beginning": "select id from client as c where id = 100000;",
-	//			//"not existing key at the end":       "select id from client as c where id = 100000;",
-	//			"lookup_and_agg": "select count(*) from order_detail as od where order_id = 1;",
-	//		},
-	//		"mssql-19": {
-	//			//"first key":                         "select id from client as c where id = 0;",
-	//			//"middle key":                        "select id from client as c where id = 5000;",
-	//			//"last key":                          "select id from client as c where id = 9999;",
-	//			//"not existing key at the beginning": "select id from client as c where id = 100000;",
-	//			//"not existing key at the end":       "select id from client as c where id = 100000;",
-	//			"lookup_and_agg": "select count(*) from order_detail as od where order_id = 1;",
-	//		},
-	//	},
-	//	QueryInt,
-	//	3000,
-	//},
-	//{
-	//	"02 - lookup by primary key + column not in index",
-	//	map[string]map[string]string{
-	//		"mysql": {
-	//			"": "select id, name from client as c where id = 1;",
-	//		},
-	//		"postgres": {
-	//			"": "select id, name from client as c where id = 1;",
-	//		},
-	//		"mssql2022": {
-	//			"": "select id, name from client as c where id = 1;",
-	//		},
-	//	},
-	//	QueryIntAndString,
-	//	3000,
-	//},
-	//{
-	//	"03 - min and max",
-	//	map[string]map[string]string{
-	//		"mysql": {
-	//			"min":     "select min(id) from client as c;",
-	//			"max":     "select min(id) from client as c;",
-	//			"min-max": "select min(id) + max(id) from client as c;",
-	//		},
-	//		"postgres": {
-	//			"min":     "select min(id) from client as c;",
-	//			"max":     "select min(id) from client as c;",
-	//			"min-max": "select min(id) + max(id) from client as c;",
-	//		},
-	//		"mssql2022": {
-	//			"min":     "select min(id) from client as c;",
-	//			"max":     "select min(id) from client as c;",
-	//			"min-max": "select min(id) + max(id) from client as c;",
-	//		},
-	//	},
-	//	QueryInt,
-	//	3000,
-	//},
-	{
-		"04 - index seek with complex condition",
+var Tests = map[string]testData{
+	"01": {
+		"lookup by primary key",
 		map[string]map[string]string{
-			"mysql": {
-				"":                  "select count(*) from client where id >= 1 and id < 10000 and id > 9990;",
-				"bigger range":      "select count(*) from order_detail where order_id >= 1 and order_id < 10000 and order_id < 2;",
-				"much bigger range": "select count(*) from order_detail where order_id >= 1 and order_id < 100000 and order_id < 2;",
+			MySql: {
+				"first key": "select id from client as c where id = 0;",
+				//"middle key":                        "select id from client as c where id = 5000;",
+				"last key": "select id from client as c where id = 9999;",
+				//"not existing key at the beginning": "select id from client as c where id = 100000;",
+				//"not existing key at the end":       "select id from client as c where id = 100000;",
+				"lookup_and_agg": "select count(*) from order_detail as od where order_id = 1;",
 			},
-			"postgres": {
-				"":                  "select count(*) from client where id >= 1 and id < 10000 and id > 9990;",
-				"bigger range":      "select count(*) from order_detail where order_id >= 1 and order_id < 10000 and order_id < 2;",
-				"much bigger range": "select count(*) from order_detail where order_id >= 1 and order_id < 100000 and order_id < 2;",
+			PostgreSql: {
+				"first key": "select id from client as c where id = 0;",
+				//"middle key":                        "select id from client as c where id = 5000;",
+				"last key": "select id from client as c where id = 9999;",
+				//"not existing key at the beginning": "select id from client as c where id = 100000;",
+				//"not existing key at the end":       "select id from client as c where id = 100000;",
+				"lookup_and_agg": "select count(*) from order_detail as od where order_id = 1;",
 			},
-			"mssql-22": {
-				"":                  "select count(*) from client where id >= 1 and id < 10000 and id > 9990;",
-				"bigger range":      "select count(*) from order_detail where order_id >= 1 and order_id < 10000 and order_id < 2;",
-				"much bigger range": "select count(*) from order_detail where order_id >= 1 and order_id < 100000 and order_id < 2;",
+			MsSql19: {
+				"first key": "select id from client as c where id = 0;",
+				//"middle key":                        "select id from client as c where id = 5000;",
+				"last key": "select id from client as c where id = 9999;",
+				//"not existing key at the beginning": "select id from client as c where id = 100000;",
+				//"not existing key at the end":       "select id from client as c where id = 100000;",
+				"lookup_and_agg": "select count(*) from order_detail as od where order_id = 1;",
 			},
-			"mssql-19": {
-				"":                  "select count(*) from client where id >= 1 and id < 10000 and id > 9990;",
-				"bigger range":      "select count(*) from order_detail where order_id >= 1 and order_id < 10000 and order_id < 2;",
-				"much bigger range": "select count(*) from order_detail where order_id >= 1 and order_id < 100000 and order_id < 2;",
+			MsSql22: {
+				"first key": "select id from client as c where id = 0;",
+				//"middle key":                        "select id from client as c where id = 5000;",
+				"last key": "select id from client as c where id = 9999;",
+				//"not existing key at the beginning": "select id from client as c where id = 100000;",
+				//"not existing key at the end":       "select id from client as c where id = 100000;",
+				"lookup_and_agg": "select count(*) from order_detail as od where order_id = 1;",
 			},
 		},
 		QueryInt,
+		3000,
+	},
+	"02": {
+		"lookup by primary key + column not in index",
+		map[string]map[string]string{
+			MySql: {
+				"": "select id, name from client as c where id = 1;",
+			},
+			PostgreSql: {
+				"": "select id, name from client as c where id = 1;",
+			},
+			MsSql19: {
+				"": "select id, name from client as c where id = 1;",
+			},
+			MsSql22: {
+				"": "select id, name from client as c where id = 1;",
+			},
+		},
+		QueryIntAndString,
+		3000,
+	},
+	"03": {
+		"min and max",
+		map[string]map[string]string{
+			MySql: {
+				"min":     "select min(id) from client as c;",
+				"max":     "select min(id) from client as c;",
+				"min-max": "select min(id) + max(id) from client as c;",
+			},
+			PostgreSql: {
+				"min":     "select min(id) from client as c;",
+				"max":     "select min(id) from client as c;",
+				"min-max": "select min(id) + max(id) from client as c;",
+			},
+			MsSql19: {
+				"min":     "select min(id) from client as c;",
+				"max":     "select min(id) from client as c;",
+				"min-max": "select min(id) + max(id) from client as c;",
+			},
+			MsSql22: {
+				"min":     "select min(id) from client as c;",
+				"max":     "select min(id) from client as c;",
+				"min-max": "select min(id) + max(id) from client as c;",
+			},
+		},
+		QueryInt,
+		3000,
+	},
+	"04": {
+		"index seek with complex condition",
+		map[string]map[string]string{
+			MySql: {
+				"":                  "select count(*) from client where id >= 1 and id < 10000 and id < 2;",
+				"bigger range":      "select count(*) from order_detail where order_id >= 1 and order_id < 10000 and order_id < 2;",
+				"much bigger range": "select count(*) from order_detail where order_id >= 1 and order_id < 100000 and order_id < 2;",
+				"fixed":             "select count(*) from order_detail where order_id >= 1 and order_id < 2 and order_id < 100000;",
+			},
+			PostgreSql: {
+				"":                  "select count(*) from client where id >= 1 and id < 10000 and id < 2;",
+				"bigger range":      "select count(*) from order_detail where order_id >= 1 and order_id < 10000 and order_id < 2;",
+				"much bigger range": "select count(*) from order_detail where order_id >= 1 and order_id < 100000 and order_id < 2;",
+				"fixed":             "select count(*) from order_detail where order_id >= 1 and order_id < 2 and order_id < 100000;",
+			},
+			MsSql19: {
+				"":                  "select count(*) from client where id >= 1 and id < 10000 and id < 2;",
+				"bigger range":      "select count(*) from order_detail where order_id >= 1 and order_id < 10000 and order_id < 2;",
+				"much bigger range": "select count(*) from order_detail where order_id >= 1 and order_id < 100000 and order_id < 2;",
+				"fixed":             "select count(*) from order_detail where order_id >= 1 and order_id < 2 and order_id < 100000;",
+			},
+			MsSql22: {
+				"":                  "select count(*) from client where id >= 1 and id < 10000 and id < 2;",
+				"bigger range":      "select count(*) from order_detail where order_id >= 1 and order_id < 10000 and order_id < 2;",
+				"much bigger range": "select count(*) from order_detail where order_id >= 1 and order_id < 100000 and order_id < 2;",
+				"fixed":             "select count(*) from order_detail where order_id >= 1 and order_id < 2 and order_id < 100000;",
+			},
+		},
+		QueryInt,
+		200,
+	},
+	"05": {
+		"nonclustered index seek vs. scan",
+		map[string]map[string]string{
+			MySql: {
+				"":   "select min(name) from client where country = 'FR';",
+				"CY": "select min(name) from client where country = 'CY';",
+				"US": "select min(name) from client where country = 'US';",
+				//">=US": "select min(name) from client where country >= 'US';",
+			},
+			PostgreSql: {
+				"":   "select min(name) from client where country = 'FR';",
+				"CY": "select min(name) from client where country = 'CY';",
+				"US": "select min(name) from client where country = 'US';",
+			},
+			MsSql19: {
+				"":   "select min(name) from client where country = 'FR';",
+				"CY": "select min(name) from client where country = 'CY';",
+				"US": "select min(name) from client where country = 'US';",
+				//"forceseek":    "select min(name) from client with (forceseek) where country = 'FR';",
+				//"forceseek-CY": "select min(name) from client with (forceseek) where country = 'CY';",
+				//"forceseek-US": "select min(name) from client with (forceseek) where country = 'US';",
+			},
+			MsSql22: {
+				"":   "select min(name) from client where country = 'FR';",
+				"CY": "select min(name) from client where country = 'CY';",
+				"US": "select min(name) from client where country = 'US';",
+				//"forceseek":    "select min(name) from client with (forceseek) where country = 'FR';",
+				//"forceseek-CY": "select min(name) from client with (forceseek) where country = 'CY';",
+				//"forceseek-US": "select min(name) from client with (forceseek) where country = 'US';",
+			},
+		},
+		QueryString,
 		200,
 	},
 	//{
@@ -149,6 +200,23 @@ func QueryInt(ctx context.Context, db *sql.DB, query string) {
 	if err := db.QueryRowContext(ctx, query).Scan(&i); err != nil {
 		if err == sql.ErrNoRows {
 			i = -1
+		} else {
+			log.Fatalf("unable to execute query: %v", err)
+		}
+	}
+	//log.Println("result = ", result)
+}
+
+func QueryString(ctx context.Context, db *sql.DB, query string) {
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	// execute query with context and handle no rows error
+
+	var s string
+	if err := db.QueryRowContext(ctx, query).Scan(&s); err != nil {
+		if err == sql.ErrNoRows {
+			s = "N/A"
 		} else {
 			log.Fatalf("unable to execute query: %v", err)
 		}
