fec6e0f19cc1975b0c6f1a25eddc2b6e6e5bcc46
Soccan <soccypowa@icloud.com>
2025-06-27 13:25:26 +0200

    Update to get MSSQL to pre-populate data

--- demo/mssql_init.sh
+++ demo/mssql_init.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+/opt/mssql/bin/sqlservr &
+
 echo "executing..."
 for i in {1..60};
 do
-    /opt/mssql-tools/bin/sqlcmd -S "tcp:localhost,1434" -U SA -P "myStrong(!)Password" -d master -i /mssql_init.sql
+    /opt/mssql-tools18/bin/sqlcmd -S "tcp:localhost,1433" -U SA -P "myStrong(!)Password" -N o -d master -i /mssql_init.sql
     if [ $? -eq 0 ]
     then
         echo "mssql_init.sql completed"
@@ -11,3 +14,5 @@ do
         sleep 1
     fi
 done
+
+tail -f
